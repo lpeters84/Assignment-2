9a38254da57475711b33aecd26df09b5c5844d0a
lpeters84 <110253865+lpeters84@users.noreply.github.com>
2022-08-02 21:33:39 -0700

    Add files via upload

---
+++ CBC Assignment 2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    public static int Main(string[] args)
+    {
+        int n = 5;
+        int[] falcon = { 0, 1, 2, 3, 4 };
+
+        int m = 100;
+        int[] bar = new int[100];
+
+        for (int i = 0; i < m; i++)
+        {
+            bar[i] = i;
+        }
+
+        Console.WriteLine($"the total number of even numbers in falcon are: {CountEvenNum(falcon, n)}");
+        Console.WriteLine($"the total number of even numbers in bar are: {CountEvenNum(bar, m)}");
+
+        return 0;
+
+    }
+
+    static int CountEvenNum(int[] arr, int size)
+    {
+        int count = 0;
+        int j;
+
+        for (j = 0; j < size; j++)
+        {
+            if (arr[j] % 2 == 0)
+            {
+                count++;
+            }
+
+        }
+        return count;
+    }
+}
